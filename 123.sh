003fde9b37adb395a91d0d7713d70505d312b244
Ubuntu <ubuntu@ip-172-31-46-33.eu-north-1.compute.internal>
2025-07-23 04:29:21 +0000

    new content added

--- 123.sh
+++ 123.sh
@@ -1,1 +1,3 @@
-Git is easy to learn and has a tiny footprint with lightning fast performance. It outclasses SCM tools like Subversion, CVS, Perforce, and ClearCase with 
+Git is easy to learn and has a tiny footprint with lightning fast performance. It outclasses SCM.
+
+this is new content
